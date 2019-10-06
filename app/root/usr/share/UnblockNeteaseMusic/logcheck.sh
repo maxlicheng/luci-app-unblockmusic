9e693f9148537e87d4139a01c9b18b946c052380
CN_SZTL <cnsztl@gmail.com>
2019-10-06 17:27:20 +0800

    [Revert 8d9c1b] fix logcheck.sh

--- app/root/usr/share/UnblockNeteaseMusic/logcheck.sh
+++ app/root/usr/share/UnblockNeteaseMusic/logcheck.sh
@@ -3,6 +3,9 @@
 log_max_size="4" #使用KB计算
 log_file="/tmp/unblockmusic.log"
 
-(( log_size = "$(ls -l "${log_file}" | awk -F ' ' '{print $5}')" / "1024" ))
-(( "${log_size}" >= "${log_max_size}" )) && echo "" > "${log_file}"
-
+while true
+do
+	(( log_size = "$(ls -l "${log_file}" | awk -F ' ' '{print $5}')" / "1024" ))
+	(( "${log_size}" >= "${log_max_size}" )) && echo "" > /tmp/unblockmusic.log
+	sleep 10m
+done
